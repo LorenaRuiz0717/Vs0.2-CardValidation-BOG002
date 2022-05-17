7a1d600611c3cdeaf442af587ec346910e96dcef
Lorena Ruiz D <lorena.ruiz@jamgo.tech>
2022-05-17 09:47:49 -0500

    update products

--- src/data.js
+++ src/data.js
@@ -33,8 +33,44 @@ export default{
         {
             'id':'6',
             'producto': 'Cueva León',
-            'precio':'$110.000',
+            'precio':'110000',
             'imagen': 'IMG/galeria/producto6.png'
+        },
+        {
+            'id':'7',
+            'producto': 'Kit small basico',
+            'precio':'38000',
+            'imagen': 'IMG/galeria/producto7.png'
+        },
+        {
+            'id':'8',
+            'producto': 'Kit confort gatuno',
+            'precio':'80000',
+            'imagen': 'IMG/galeria/producto8.png'
+        },
+        {
+            'id':'9',
+            'producto': 'Kit confort didactico',
+            'precio':'85000',
+            'imagen': 'IMG/galeria/producto9.png'
+        },
+        {
+            'id':'10',
+            'producto': 'Kit basico rosa',
+            'precio':'99000',
+            'imagen': 'IMG/galeria/producto10.png'
+        },
+        {
+            'id':'11',
+            'producto': 'Arenero y rascador',
+            'precio':'103000',
+            'imagen': 'IMG/galeria/producto11.png'
+        },
+        {
+            'id':'12',
+            'producto': 'Rascador duplex',
+            'precio':'130000',
+            'imagen': 'IMG/galeria/producto12.png'
         }
     ]
 };
